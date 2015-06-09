47bde36d7d190935383e0cfa33b205e0a2d80f74
Cassiohg <cassiohg@gmail.com>
2015-06-09 05:28:36 -0300

    added a hello world return json message

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Top View listening at http://%s:%s', host, port);
+
+});
+
+// respond with "Hello World!" on the homepage
+app.use('/', function (req, res, next) {
+	console.log(' - a request has arrived:', req.originalUrl);
+	next();
+});
+
+// accept POST request on the homepage
+app.get('/person/:id', function (req, res, next) {
+	// var newPerson = new Person(newPersonID)
+
+	json.send("{'message': 'welcome to top view server'}")
+});
+
+// accept POST request on the homepage
+app.get('/person/:id', function (req, res, next) {
+	// var newPerson = new Person(newPersonID)
+
+	json.send("{}")
+});
+
+// accept PUT request at /user
+app.put('/person/:geolocation', function (req, res) {
+  res.send('Got a PUT request at /person/geolocation');
+});
